c413ccca64992b6b8afc6f3c88f8dd93528add02
Andjelko28 <105885932+Andjelko28@users.noreply.github.com>
2024-05-27 14:29:05 +0200

    "Added DB connection initialization to app.ts and updated tsconfig.json with experimental decorators and metadata emission"

--- app.ts
+++ app.ts
@@ -1,10 +1,19 @@
 import express from 'express';
 
+// Import of DB connection
+import dbConnection from './common/db-connection';
 
-const app  = express();
+// Performing connection to database
+dbConnection.initialize()
+    .then(() => { console.log('Connected to DB!') })
+    .catch((err: any) => {
+        console.log(err);
+    })
+
+const app = express();
 app.use(express.json());
 
 
-app.listen(3000,()=>{
+app.listen(3000, () => {
     console.log('Server is listening at port 3000;');
-})
+}) 
