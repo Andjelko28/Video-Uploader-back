cee89e0479478f085873fc59dece2b162540dc32
Andjelko28 <105885932+Andjelko28@users.noreply.github.com>
2024-05-29 14:14:56 +0200

    - Created function for user-controller.ts

---
+++ controller/user-controller.ts
@@ -0,0 +1,7 @@
+import userService from "../service/user-service";
+import { Request, Response } from "express";
+
+const register = async (req: Request, res: Response) => {
+    const result = await userService.register(req.body);
+    res.send(result);
+}
